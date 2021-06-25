cb8e7a4ba737d7adcb8bf4fef26aa5434c643829
cdunn95 <cameron@Camerons-MacBook-Air.local>
2021-06-25 14:45:07 -0400

    Add react-navigation/native to project

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './Screens/HomeScreen.js';
+
 
 export default function App() {
+
+  const Stack = createStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home' headerMode='none'>
+        <Stack.Screen name='Home' component={HomeScreen} />
+      </Stack.Navigator>
+      <StatusBar backgroundColor={'rgba(0,0,0,0)'} barStyle={'dark-content'} />
+    </NavigationContainer>
   );
 }
 
@@ -16,6 +23,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
